0ace8eac2f2f072d9089451f877e30f09fdde2f5
Jean Juang <jjuang@princeton.edu>
2015-01-01 22:54:19 -0700

    working heatmap

--- csv_parse.py
+++ csv_parse.py
@@ -7,12 +7,28 @@ with open('original2.csv', 'r+Ub') as csvfile:
 	for row in reader:
 		counter = 0
 		day_average = 0
-		firsttwo = 1;
-		toWrite = "";
+		firsttwo = 1
+
+		day = 1;
+
+		name = ""
+		longitude = 0
+		latitude = 0
+		x = ""
+
 		for word in row:
-			if firsttwo >= 1 and firsttwo <= 4:
-				print word + ",",
-				firsttwo = firsttwo + 1
+			if firsttwo == 1:
+			 name = word
+			 firsttwo +=1;
+			elif firsttwo == 2:
+			 longitude = word
+			 firsttwo +=1;
+			elif firsttwo == 3:
+			 latitude = word
+			 firsttwo +=1;
+			elif firsttwo == 4:
+			 x = word
+			 firsttwo +=1;
 			else:
 				counter+=1
 				if word.isdigit():
@@ -20,9 +36,11 @@ with open('original2.csv', 'r+Ub') as csvfile:
 				else:
 					day_average += 0
 				if counter == 24:
-					print str(day_average/1000000) + ",",
+					print name + "," + longitude + "," + latitude + "," + x + "," + str(day) + "," + str(day_average/100000000.0)
+
+					day += 1
 					day_average = 0
 					counter = 0
-		print '\r\n'
+		
 
 
